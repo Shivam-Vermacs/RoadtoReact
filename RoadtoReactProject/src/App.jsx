8193f69642a33cb9adc52ee82a6b75f0ee07c22d
Shivam-Vermacs <verma.shivam.cs@gmail.com>
2025-09-08 04:58:15 +0530

    Road to React

---
+++ RoadtoReactProject/src/App.jsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+
+const title = "Hello";
+const App = () => {
+  const data = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  return (
+    <>
+      <div>Hello {title}</div>
+      <Search />
+      <hr />
+      <List list={data} />
+    </>
+  );
+};
+
+const Search = () => {
+  const HandleChange = (e) => {
+    console.log(e);
+    console.log(e.target.value);
+  };
+
+  return (
+    <div>
+      {" "}
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" onChange={HandleChange} />
+    </div>
+  );
+};
+const List = (props) => {
+  return (
+    <div>
+      <ul>
+        {props.list.map((item) => {
+          return <Item key={item.objectID} item={item} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+const Item = (props) => {
+  return (
+    <div>
+      <li>
+        <span>
+          <a href={props.item.url}>{props.item.title}</a>
+        </span>
+        <span>{props.item.author}</span>
+        <span>{props.item.num_comments}</span>
+      </li>
+    </div>
+  );
+};
+export default App;
